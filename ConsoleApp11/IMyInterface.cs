73bdd6db2027c673d49b154eba3f19668f6a0a96
Paul Michaels <pcmichaels@live.com>
2022-09-05 19:16:23 +0100

    Example project

---
+++ ConsoleApp11/IMyInterface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal interface IMyInterface
+    {
+        void Test();
+
+        public string TestId { get; }
+    }
+
+    internal class MyService1 : IMyInterface
+    {
+        public string TestId => "1";
+
+        public void Test()
+        {
+            Console.WriteLine("My Service 1");
+        }
+    }
+
+    internal class MyService2 : IMyInterface
+    {
+        public string TestId => "2";
+
+        public void Test()
+        {
+            Console.WriteLine("My Service 2");
+        }
+    }
+
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleApp11;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+        services.AddScoped<IMyInterface, MyService1>()
+                .AddScoped<IMyInterface, MyService2>())          
+    .Build();
+
+
+RunTest(host.Services);
+
+await host.RunAsync();
+
+
+static void RunTest(IServiceProvider services)
+{   
+    var foundServices = services.GetServices<IMyInterface>();
+    var service = foundServices.First(a => a.TestId == "1");
+    service.Test();
+    
+}
